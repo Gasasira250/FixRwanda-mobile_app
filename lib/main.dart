8b7f1b4fb8c0b9448d0111cea7afb7ec019d88c7
Hannington Kasasira <kasasiradon@gmail.com>
2026-09-20 20:25:12 +0200

    Add a public Android install page and keep web routing off mobile builds.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'app.dart';
 import 'data/local_marketplace_store.dart';
 import 'state/marketplace_controller.dart';
+import 'web_url_strategy_stub.dart'
+    if (dart.library.html) 'web_url_strategy_web.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    setUrlStrategy(HashUrlStrategy());
-  }
+  configureUrlStrategy();
   final store = LocalMarketplaceStore();
   await store.initialize();
   final controller = MarketplaceController(store);
